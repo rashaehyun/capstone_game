38504fbbe31e1f6d857e84d91e98b74e977f3b29
rashaehyun <rsh8412@gmail.com>
2025-03-29 00:42:32 +0900

    run&jump

--- Assets/script/Player_Jump.cs
+++ Assets/script/Player_Jump.cs
@@ -2,55 +2,59 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
-    [SerializeField]
-    float jumpPower;
-    [SerializeField]
-    bool isGround;
+    [SerializeField] float jumpPower = 5f;
+    [SerializeField] int maxJumpCount = 2;
 
-    private int jumpCount = 0;  // 점프 횟수를 추적할 변수
-    private int maxJumpCount = 2; // 최대 점프 횟수
+    private bool isGround = false;
+    private int jumpCount = 0;
+    private bool isJump = false;
 
-    Rigidbody2D body;
-    Animator anim;
+    private Rigidbody2D body;
+    private Animator anim;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        
     }
 
     private void LateUpdate()
     {
         anim.SetBool("Ground", isGround);
+        anim.SetBool("isJump", isJump);
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        // "Ground" 태그를 가진 오브젝트와 접촉했을 때
         if (collision.CompareTag("Ground"))
         {
-            // 바닥에 닿으면 점프 횟수 리셋
-            if (jumpCount > 0)
-                jumpCount = 0;
-            isGround = true; // 바닥에 있을 때 isGround를 true로 설정
+            isGround = true;
+            jumpCount = 0;
+            isJump = false;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        // 바닥을 떠나면 isGround를 false로 설정
         if (collision.CompareTag("Ground"))
+        {
             isGround = false;
+        }
     }
 
-    private void OnJump()
+    
+    public void OnJump()
     {
-        if ((isGround && jumpCount == 0) || jumpCount == 1)  // 점프 횟수가 0이거나 1일 때만 점프
+        if (jumpCount < maxJumpCount)
         {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, 0f); 
             body.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-            jumpCount++;  // 점프 횟수 증가
+            jumpCount++;
+            isGround = false;
 
-            isGround = false;  // 점프할 때는 isGround를 false로 설정
+            isJump = true;
         }
+        
     }
 }
--- Assets/script/player_move.cs
+++ Assets/script/player_move.cs
@@ -10,11 +10,13 @@ public class Player_Move : MonoBehaviour
 
     private Rigidbody2D body;
     private SpriteRenderer spriteRenderer; // 스프라이트 렌더러 추가
+    private Animator anim;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>(); // SpriteRenderer 가져오기
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -23,9 +25,16 @@ public class Player_Move : MonoBehaviour
 
         // 캐릭터 방향 전환
         if (inputValue > 0)
+        { 
             spriteRenderer.flipX = false; // 오른쪽 이동 시 원래 방향
+        }
         else if (inputValue < 0)
+        { 
             spriteRenderer.flipX = true;  // 왼쪽 이동 시 좌우 반전
+        }
+
+        bool isRunning = Mathf.Abs(inputValue) > 0.01f;
+        anim.SetBool("isRun", isRunning);
     }
 
     private void OnMove(InputValue value)
